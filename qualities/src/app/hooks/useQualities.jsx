4cb42b406d9210fa4d825b565dc3851221897b70
dimonabinsk <dimonabinsk@yandex.ru>
2022-11-29 14:50:01 +0300

    добавил updateQuality

--- qualities/src/app/hooks/useQualities.jsx
+++ qualities/src/app/hooks/useQualities.jsx
@@ -26,8 +26,30 @@ export const QualitiesProvider = ({ children }) => {
     getQualities();
   }, []);
 
+  const getQuality = (id) => {
+    return qualities.find((q) => q._id === id);
+  };
+
+  const updateQuality = async ({_id: id, ...data}) => {
+    try {
+      const { content } = await qualityService.update(id, data);
+      setQualities((prev) =>
+        prev.map((item) => {
+          if (item._id === content._id) {
+            return content;
+          }
+          return item;
+        })
+      );
+      return content;
+    } catch (e) {
+      const { message } = e.response.data;
+      setError(message);
+    }
+  };
+
   return (
-    <QualitiesContext.Provider value={{ qualities }}>
+    <QualitiesContext.Provider value={{ qualities, getQuality, updateQuality }}>
       {!isLoading ? children : <h2>Qualities Loading...</h2>}
     </QualitiesContext.Provider>
   );
--- qualities/src/app/pages/editQuality.jsx
+++ qualities/src/app/pages/editQuality.jsx
@@ -1,53 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { toast } from "react-toastify";
-import qualityService from "../services/quality.services";
+
 
 import QualityForm from "../components/ui/qualityForm";
-// import httpServices from "../services/http.services";
+import { useQualities } from "../hooks/useQualities";
+
 
 const EditQualityPage = () => {
-  const [quality, setQuality] = useState(null);
   const { id } = useParams();
+  const {getQuality, updateQuality} = useQualities();
+  const quality = getQuality(id);
+  console.log(quality);
 
-
-  const updateQuality = async (content) => {
-    try {
-      const data = await qualityService.update(id, content);
-      // console.log(data);
-      return data.content;
-    } catch (e) {
-      const { message} = e.response.data;
-      toast.error(message);
-    }
-  };
-
-  const getQuality = async (id) => {
-    try {
-      const data = await qualityService.get(id);
-      // console.log(data);
-      return data.content;
-    } catch (e) {
-      const { message} = e.response.data;
-      toast.error(message);
-    }
-  };
 
   const handleSubmit = (data) => {
     updateQuality(data);
   };
 
-  useEffect(() => {
-    getQuality(id).then((data) => setQuality(data));
-  }, [id]);
   return (
     <>
       <h1>Edit Quality Page</h1>
-      {quality !== null ? (
-        <QualityForm data={quality} onSubmit={handleSubmit} />
-      ) : (
-        <p>Loading...</p>
-      )}
+
+      <QualityForm data={quality} onSubmit={handleSubmit} />
     </>
   );
 };
